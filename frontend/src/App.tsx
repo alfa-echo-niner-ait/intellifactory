2ea176cfb1722b2110f0575b519ee644a553a1f7
alfa-echo-niner-ait <m.aaemon98@gmail.com>
2025-09-24 23:46:09 +0800

    Shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
 	return (
 		<>
-			<div className="min-h-dvh flex">
-        <div className="flex-1">
-          <h1 className="text-3xl text-center my-20 font-bold text-blue-700">
-            IntelliFacory!
-          </h1>
-        </div>
-      </div>
+			<div className="min-h-dvh flex justify-center items-center">
+				<div className="bg-gray-50 p-10">
+					<h1 className="text-3xl text-center my-20 font-bold text-blue-700 py-15">
+						IntelliFacory!
+					</h1>
+					<div className="flex flex-wrap items-center gap-2 md:flex-row">
+						<Button>Button</Button>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
